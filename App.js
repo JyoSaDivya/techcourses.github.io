a0a309586287ceb4b90ca6c5f27ed1852f8cf520
JyoSaDivya <87201437+JyoSaDivya@users.noreply.github.com>
2021-07-09 22:40:35 +0530

    React Courses Website

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import reactDom from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
